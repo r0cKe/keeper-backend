4c8b3821e3c28d55d7610e433753eba80d0bb7e2
r0cKe <rsritiksabhrwal123@mail.com>
2022-12-15 11:01:27 +0530

    inital commit

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const User = require("./Models/User.js");
-const connectToMongoDB = require("../Back-end/db.js");
-const bodyParser = require("body-parser");
+const connectToMongoDB = require("./db.js");
+// const bodyParser = require("body-parser");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	session({
@@ -46,19 +46,19 @@ app.use("/api/notes", require("./routes/notes"));
 
 // --------------Deployment --------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname1, "/front-end/build")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname1, "/front-end/build")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname1, "front-end", "build", "index.html"));
-	});
-} else {
-	app.get("/", (req, res) => {
-		res.send("API is not running successfully");
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname1, "front-end", "build", "index.html"));
+// 	});
+// } else {
+// 	app.get("/", (req, res) => {
+// 		res.send("API is not running successfully");
+// 	});
+// }
 
 // --------------Deployment --------------
 
